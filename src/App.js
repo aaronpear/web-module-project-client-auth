f6d71b7558470d0a262c2384065f4231b228baa6
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-14 11:56:51 -0800

    login component complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <div className='nav-bar'>
+          <h1>Friends Database</h1>
+          <ul>
+            <li>
+              <Link to='/login'>Login.</Link>
+            </li>
+            <li>Friendlist.</li>
+            <li>Addfriend.</li>
+            <li>
+              <Link to='/logout'>Logout</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+    const history = useHistory();
+
+    const initialState = {
+        username: '',
+        password: ''
+    }
+
+    const [credentials, setCredentials] = useState(initialState);
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const login = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:9000/api/login', credentials)
+            .then((res) => {
+                const { token, role, username } = res.data;
+                localStorage.setItem('token', token);
+                localStorage.setItem('role', role);
+                localStorage.setItem('username', username);
+                history.push('/friendslist');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <div className='login'>
+            <h2>Login</h2>
+            <form className='login-form' onSubmit={login}>
+                <label>Username</label>
+                <input type='text' name='username' value={credentials.username} onChange={handleChange} />
+                <label>Password</label>
+                <input type='password' name='password' value={credentials.password} onChange={handleChange} />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
